5fd142486c2f379fec7bc40dff74e5710041cdcd
Polina Chupailenko <pchup@softserveinc.com>
2019-06-05 11:14:07 +0300

    added edit popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppComponent } from './app.component';
 import { UsersModule } from './components/users/users.module';
 import { SharedModulesModule } from './SharedModules/shared-modules.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {FormsModule} from '@angular/forms';
-import {PopupComponent} from './components/popup/popup.component';
 import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
@@ -20,7 +18,6 @@ import { ContainerComponent } from './components/container/container.component';
     NgbModule,
     SharedModulesModule
   ],
-  // exports: [NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
